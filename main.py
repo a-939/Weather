0c5a64ac23d1c6423d432fb01abaa747a36e34bd
= <angelkeerthivarmabhupatiraju@gmail.com>
2024-01-07 11:03:20 +0530

    Complete Application with exe file

--- main.py
+++ main.py
@@ -1,43 +1,92 @@
 import json
 import httplib2
+import time
+
+recent_city = ''
+auto_refresh_count = 0
 
 
 # This the backend code where we fetch the weather details from server
-def err_in_response():
-    return "Server error, please try after some time."
+def err_in_response(status):
+    if status == 400 or status == 500:
+        print("Server error, please try after some time.")
+    elif status == 0:
+        print("Please select valid option")
+    elif status == 1006:
+        print("Please valid city name")
 
 
 def get_weather(city):
+    global recent_city
     url = 'https://api.weatherapi.com/v1/current.json?'
     apikey = '84196dcb99134d34baf163255240501'
-    params = '&key=' + apikey + '&q='+city
+    params = '&key=' + apikey + '&q=' + city
     h = httplib2.Http('.cache')
-    response, content = h.request(url+params)
+    response, content = h.request(url + params)
+    # print(url + params)
+    # print(response.status)
     if response.status == 200:
+        recent_city = city
         return get_weather_data(content)
-    else: 
-        return err_in_response()
+    else:
+        return err_in_response(response.status)
 
 
 # This method to get the import values from the response
 def get_weather_data(response):
     data = json.loads(response)
-    # TODO: Here You can print what ever details you want from the response
-    print(data['location']['name'])
-    # TO check the structure print data and check in the console
+    print()
+    print(data['location']['name'], data['location']['region'], data['location']['country'], sep=',')
+    print(data['current']['condition']['text'])
+    print()
+    auto_refresh()
 
 
-# TODO: create this method automatic refresh every 15-30 sec
 def auto_refresh():
-    pass
+    global auto_refresh_count
+    if auto_refresh_count < 1:
+        auto_refresh_count += 1
+        time.sleep(15)
+        get_weather(recent_city)
+    else:
+        auto_refresh_count = 0
 
 
-# TODO: create the UI
 # This is UI main method called after start the execution
 def front_end():
-    print()
+    cities_list = ['Nancy', 'San Francisco',
+                   'Nandyal', 'Kurnool', 'kadapa']
+    while True:
+        user_input = ''
+        print()
+        while True:
+            print()
+            print('Welcome to the weather checking Console application Dev kiru')
+            print('Please select:')
+            print('1. Type city name')
+            print('2. Select city name in favorite city list')
+            user_selection = input()
+            if user_selection == '1' or user_selection == '2':
+                user_input = user_selection
+                break
+            else:
+                err_in_response(0)
+        if user_input == '1':
+            city = input('Enter city name: ')
+            get_weather(city)
+        elif user_input == '2':
+            print('Please select city name in favorite city list below')
+            print('Favorite city List : ')
+            print(*enumerate(cities_list))
+            city_number = int(input('Enter city number: '))
+            get_weather(cities_list[city_number])
+
+        if recent_city == '' or recent_city not in cities_list:
+            cities_list.append(recent_city)
 
 
 # It starts the main execution
 if __name__ == '__main__':
-    get_weather("nandyal")
+    front_end()
+    if recent_city != '':
+        auto_refresh()
